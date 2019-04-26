a5c7f6f1b63f0498cc56e21d92724e518aeb4ab7
lgarczyn <lgarczyn@student.42.fr>
2019-04-26 20:28:33 +0200

    fixed read_tetri

---
+++ read_tetri.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   read_tetri.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgarczyn <lgarczyn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/26 17:45:52 by brjorgen          #+#    #+#             */
+/*   Updated: 2019/04/26 20:28:04 by lgarczyn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+#include "get_next_line.h"
+#include "libft.h"
+
+bool				check_endline(int fd)
+{
+	char	*line;
+
+	if (get_next_line(fd, &line) <= 0 || line[0] != '\n')
+	{
+		free(line);
+		return (false);
+	}
+	free(line);
+	return (true);
+}
+
+bool				read_line(char *line, t_block *blocks)
+{
+	int		line_pos;
+
+	line_pos = -1;
+	while (++line_pos < 4)
+	{
+		if (line[line_pos] == '.')
+			blocks[line_pos] = b_empty;
+		else if (line[line_pos] == '#')
+			blocks[line_pos] = b_filled;
+		else
+			return (false);
+	}
+	if (line[4] != '\n')
+		return (false);
+	return (true);
+}
+
+bool				read_tetri(int fd, t_tetri *out)
+{
+	int		line_count;
+	char	*line;
+	bool	res;
+
+	line_count = -1;
+	while (++line_count < 4 && get_next_line(fd, &line) > 0)
+	{
+		res = read_line(line, &(out->data[line_count]));
+		free(line);
+		if (res == false)
+			return (false);
+	}
+	if (line_count != 4)
+		return (false);
+	check_endline(fd);
+	out->used = false;
+	return (true);
+}
