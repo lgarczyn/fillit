1199df7ff615e8f27eb309be10541f28dbd90f5d
Brian JORGENSEN <brjorgen@student.42.fr>
2019-04-26 19:57:26 +0200

    read_tetri renamed read_file: should work, hopefully

--- read_file.c
+++ read_file.c
@@ -6,7 +6,7 @@
 /*   By: brjorgen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/26 17:45:52 by brjorgen          #+#    #+#             */
-/*   Updated: 2019/04/26 18:17:46 by brjorgen         ###   ########.fr       */
+/*   Updated: 2019/04/26 19:55:30 by brjorgen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,39 @@
 #include "get_next_line.h"
 #include "libft.h"
 
+/*
+typedef enum		e_block
+{
+	b_empty = 0,
+	b_filled = 1,
+}					t_block;
+*/
+
 bool				read_tetri(int fd, t_tetri *out)
 {
-	int		line_counter;
+	t_block tetri;
 	int		line_pos;
-	int		touching;
+	int		line_count;
 	char	**line;
 
-	line_counter = 0;
 	line_pos = 0;
-	while (get_next_line(fd, &line))
-		line_counter++;
-	if (line_counter > 3)
-	{
-		free(line);
-		return (false);
-	}
-	free(line);
-	while (line_counter++ < 4)
+	line_count_check(fd);
+	while (line_count++ < 4)
 	{
 		get_next_line(fd, &line);
 		while (line_pos < 4)
 		{
-			if (line[3] != '\n' || line[line_pos] != '.' || line[line_pos] != '#')
+			if (line[line_pos] == '.')
+				out->data[line_count][line_pos] = b_empty;
+			if (line[line_pos] == '#')
+				out->data[line_count][line_pos] = b_used;
+			if (line[4] != '\n')
 			{
 				free(line);
 				return (false);
 			}
-			line_pos++;
 		}
-		line_pos = 0;
+		line_count = 0;
 	}
+	return (true);
 }
