3d487b8208c3790ea2ce21da2a28ad87c8f4b9b7
Brian JORGENSEN <brjorgen@student.42.fr>
2019-04-26 20:04:28 +0200

    Fixed errors in read_file.c

--- read_file.c
+++ read_file.c
@@ -6,7 +6,7 @@
 /*   By: brjorgen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/26 17:45:52 by brjorgen          #+#    #+#             */
-/*   Updated: 2019/04/26 19:55:30 by brjorgen         ###   ########.fr       */
+/*   Updated: 2019/04/26 20:04:05 by brjorgen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,23 +24,23 @@ typedef enum		e_block
 
 bool				read_tetri(int fd, t_tetri *out)
 {
-	t_block tetri;
 	int		line_pos;
 	int		line_count;
 	char	**line;
 
 	line_pos = 0;
-	line_count_check(fd);
+	line_count = 0;
+	line = NULL;
 	while (line_count++ < 4)
 	{
-		get_next_line(fd, &line);
-		while (line_pos < 4)
+		get_next_line(fd, line);
+		while (line_pos++ < 4)
 		{
-			if (line[line_pos] == '.')
+			if (*line[line_pos] == '.')
 				out->data[line_count][line_pos] = b_empty;
-			if (line[line_pos] == '#')
-				out->data[line_count][line_pos] = b_used;
-			if (line[4] != '\n')
+			if (*line[line_pos] == '#')
+				out->data[line_count][line_pos] = b_filled;
+			if (*line[4] != '\n')
 			{
 				free(line);
 				return (false);
