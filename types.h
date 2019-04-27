c03ba4ab0fdfa47d357fb85a4540e434a88fd3c7
lgarczyn <lgarczyn@student.42.fr>
2019-04-27 02:54:44 +0200

    added protection to types.h

--- types.h
+++ types.h
@@ -6,9 +6,15 @@
 /*   By: lgarczyn <lgarczyn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/27 00:38:33 by lgarczyn          #+#    #+#             */
-/*   Updated: 2019/04/27 00:40:00 by lgarczyn         ###   ########.fr       */
+/*   Updated: 2019/04/27 02:54:29 by lgarczyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef __TYPES_H
+# define __TYPES_H
+
 typedef unsigned int	t_score;
 typedef unsigned int	t_coord;
+typedef unsigned char	t_tet_coord;
+
+#endif
